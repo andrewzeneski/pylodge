1b520136bd4cd612ed452c48212beaf0e317bfe4
akondapalli <ashwin@gettalent.com>
2015-08-17 14:39:21 -0700

    changes to the project structure

--- setup.py
+++ setup.py
@@ -2,25 +2,16 @@ __author__ = 'ashwin'
 """A Test Lodge based pylodge module.
 
 """
-
 from setuptools import setup, find_packages
-from codecs import open
-from os import path
 from pylodge import make_config
 
 
 
-here = path.abspath(path.dirname(__file__))
-
-with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
-    long_description = f.read()
-
 setup(
     name='pylodge',
-    version='0.1',
+    version='0.1.5',
 
     description='Test Automation framework for TestLodge',
-    long_description=long_description,
 
     url='https://github.com/gettalent/pylodge',
 
@@ -36,15 +27,16 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Quality Assurance',
+        'Programming Language :: Python',
         'License :: OSI Approved :: MIT License',
 
     ],
 
     keywords='TestLodge, test automation',
 
-    packages=find_packages(),
+    packages=['pylodge'],
 
-    install_requires=['pyOpenSSL', 'ndg-httpsclient', 'pyasn1', 'requests'],
+    install_requires=['requests'],
 
 )
 make_config.create_config_file()
